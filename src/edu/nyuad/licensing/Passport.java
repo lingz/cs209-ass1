c313c2f80de69e7f8d57f77478396cf8a6020b90
Anthony Spalvieri-Kruse <ask417@nyu.edu>
2014-02-20 12:11:10 +0400

    Passport.java

---
+++ src/edu/nyuad/licensing/Passport.java
@@ -0,0 +1,10 @@
+package assignment1;
+
+import java.util.Date;
+
+public class Passport extends AbstractPrimaryDocument {
+    public Passport(String firstName, String lastName, String nationality,
+                    char gender, Date dateOfBirth, Date expiryDate) {
+        super(firstName,lastName,nationality,gender,dateOfBirth,expiryDate);
+    }
+}
