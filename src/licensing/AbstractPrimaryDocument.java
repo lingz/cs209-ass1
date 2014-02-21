b7a49e7b3fdc495f66eaad2e45f489fde3950a07
Anthony Spalvieri-Kruse <ask417@nyu.edu>
2014-02-21 16:10:25 +0400

    packages removed

--- src/licensing/AbstractPrimaryDocument.java
+++ src/licensing/AbstractPrimaryDocument.java
@@ -1,4 +1,3 @@
-package assignment1;
 
 import java.util.Date;
 
---
+++ src/licensing/SynchronizedQueue.java
@@ -0,0 +1,9 @@
+
+class SynchronizedQueue {
+	public void add(Customer customer){}
+
+	public Customer remove(){
+	return new Customer(null,null,null,null,null);
+	}
+}
+
